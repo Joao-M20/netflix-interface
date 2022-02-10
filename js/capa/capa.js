fdc9fa153e1409eb75602bafd320272e4641b569
IvanJoao <ivanmayta2585@gmail.com>
2022-02-10 19:54:37 -0300

    first commit

---
+++ js/capa/capa.js
@@ -0,0 +1,17 @@
+const tirando = document.getElementById('tirando');
+//const cor = tirando;
+//const tela = principal.style.maxWidth(600);
+//const telinha = tirando.style.maxWidth(600);
+const principal = document.getElementsByClassName('filme-principal');
+
+
+function mudarText() {
+    if ( tirando = tirando.style.maxWidth(100) ) {
+        const texto = document.createTextNode('h1');
+        
+    }
+}
+
+mudarText();
+
+const lembrarDisso = "USAR BOTÃO PARA MUDAR O SITE PARA DISPOSITIVOS MOVEIS :D";
